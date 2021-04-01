7ea6df4651a3a4b023fcb3fea3bf5a0fea7df387
Siddhant Mittal <sidmit2001@gmail.com>
2021-04-01 11:43:19 +0530

    setup

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState,useEffect } from 'react';
+import './App.css';
+import Axios from 'axios';
+
+function App() {
+  const [name,setUserName] = useState('');
+  const [age,setAge] = useState(0);
+
+  const[userList,setUserList] = useState([]);
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/get').then((response)=>{
+      console.log(response.data);
+      setUserList(response.data);
+    })
+  }, []);
+
+  const submitButton = () => {
+      Axios.post('http://localhost:3001/api/insert',{userName: name, userAge: age});
+      setUserList([...userList,{name: name, age: age}]);
+  };
+
+  const deleteUser = (userName) => {
+    Axios.delete(`http://localhost:3001/api/delete/${userName}`);
+  };
+
+  return (
+    <div className="App">
+    <h1>SIGNUP PAGE</h1>
+    <div className="form-container">
+      <label>UserName: </label>
+      <input className="form-input" type="text" name="userName" onChange={(e)=>setUserName(e.target.value)}/>
+      <label>Age: </label>
+      <input className="form-input" type="number" name="userAge" onChange={(e)=>setAge(e.target.value)}/>
+    </div>
+    <button className="form-button" onClick={submitButton}>Submit</button>
+    {userList.map((val)=>{
+      return (
+        <div className="user-list">
+        <h1> UserName: {val.name}</h1> 
+        <p1>Age: {val.age}</p1>
+        <button onClick={()=>{deleteUser(val.name)}}>Delete</button>
+        <input type="number"/>
+        <button>Update</button>
+        </div>
+      );
+    })}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+export default db = mysql.createConnection({
+    host:'us-cdbr-east-03.cleardb.com',
+    user:'b8943fe2b8c784',
+    password:'6be4667b',
+    database:'heroku_fcfa696484b3b16',
+});
+
+
+ 
