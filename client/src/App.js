f48dba917ecc4c1bbf71afb2ffb5234a97d0f4ce
Siddhant Mittal <sidmit2001@gmail.com>
2021-04-07 21:13:50 +0530

    Merge branch 'main' of https://github.com/standmarsh/forum_web_app

--- client/src/App.js
+++ client/src/App.js
@@ -2,94 +2,131 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Axios from "axios";
 import { Button } from "@chakra-ui/react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
-function App() {
-  const [name, setUserName] = useState("");
-  const [age, setAge] = useState(0);
-  const [userList, setUserList] = useState([]);
-  const [newAge, setNewAge] = useState(0);
-
-  useEffect(() => {
-    Axios.get("/api/get").then((response) => {
-      console.log(response.data);
-      setUserList(response.data);
-    });
-  }, []);
-
-  const submitButton = () => {
-    Axios.post("/api/insert", {
-      userName: name,
-      userAge: age,
-    });
-    setUserList([...userList, { name: name, age: age }]);
-  };
-
-  const deleteUser = (userName) => {
-    Axios.delete(`/api/delete/${userName}`);
-  };
-
-  const updateAge = (userName) => {
-    Axios.put("/api/update", {
-      userName: userName,
-      userAge: newAge,
-    });
-    setNewAge("");
-  };
-
+export default function App() {
   return (
-    <div className="App">
-      <Button colorScheme="blue">Test</Button>
-      <h1>SIGNUP PAGE</h1>
-      <div className="form-container">
-        <label>UserName: </label>
-        <input
-          className="form-input"
-          type="text"
-          name="userName"
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <label>Age: </label>
-        <input
-          className="form-input"
-          type="number"
-          name="userAge"
-          onChange={(e) => setAge(e.target.value)}
-        />
+<Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            hello
+          </Route>
+          <Route path="/users">
+            hi
+          </Route>
+          <Route path="/">
+            hi
+          </Route>
+        </Switch>
       </div>
-      <button className="form-button" onClick={submitButton}>
-        Submit
-      </button>
-      {userList.map((val) => {
-        return (
-          <div className="user-list">
-            <h1> UserName: {val.name}</h1>
-            <p1>Age: {val.age}</p1>
-            <button
-              onClick={() => {
-                deleteUser(val.name);
-              }}
-            >
-              Delete
-            </button>
-            <input
-              type="number"
-              id="updateAge"
-              onChange={(e) => {
-                setNewAge(e.target.value);
-              }}
-            />
-            <button
-              onClick={() => {
-                updateAge(val.name);
-              }}
-            >
-              Update
-            </button>
-          </div>
-        );
-      })}
-    </div>
+    </Router>
   );
 }
 
-export default App;
+// const [name, setUserName] = useState("");
+//   const [age, setAge] = useState(0);
+//   const [userList, setUserList] = useState([]);
+//   const [newAge, setNewAge] = useState(0);
+
+//   useEffect(() => {
+//     Axios.get("/api/get").then((response) => {
+//       console.log(response.data);
+//       setUserList(response.data);
+//     });
+//   }, []);
+
+//   const submitButton = () => {
+//     Axios.post("/api/insert", {
+//       userName: name,
+//       userAge: age,
+//     });
+//     setUserList([...userList, { name: name, age: age }]);
+//   };
+
+//   const deleteUser = (userName) => {
+//     Axios.delete(`/api/delete/${userName}`);
+//   };
+
+//   const updateAge = (userName) => {
+//     Axios.put("/api/update", {
+//       userName: userName,
+//       userAge: newAge,
+//     });
+//     setNewAge("");
+//   };
+
+
+{/* <Button colorScheme="blue">Test</Button>
+<h1>SIGNUP PAGE</h1>
+<div className="form-container">
+  <label>UserName: </label>
+  <input
+    className="form-input"
+    type="text"
+    name="userName"
+    onChange={(e) => setUserName(e.target.value)}
+  />
+  <label>Age: </label>
+  <input
+    className="form-input"
+    type="number"
+    name="userAge"
+    onChange={(e) => setAge(e.target.value)}
+  />
+</div>
+<button className="form-button" onClick={submitButton}>
+  Submit
+</button>
+{userList.map((val) => {
+  return (
+    <div className="user-list">
+      <h1> UserName: {val.name}</h1>
+      <p1>Age: {val.age}</p1>
+      <button
+        onClick={() => {
+          deleteUser(val.name);
+        }}
+      >
+        Delete
+      </button>
+      <input
+        type="number"
+        id="updateAge"
+        onChange={(e) => {
+          setNewAge(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          updateAge(val.name);
+        }}
+      >
+        Update
+      </button>
+    </div>
+  );
+})} */}
