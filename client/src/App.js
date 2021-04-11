bfb38ac606500b410b3c008e7eb41858efdf840e
Shagun Agrawal <agrawalshagun07@gmail.com>
2021-04-11 23:28:32 +0530

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,31 @@
-
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Home from './component/HomePage/Home';
 import Header from './component/Navbar/Navbar';
 
-const breakpoints = ["360px", "768px", "1024px", "1440px"];
+const breakpoints = ['360px', '768px', '1024px', '1440px'];
 breakpoints.sm = breakpoints[0];
 breakpoints.md = breakpoints[1];
 breakpoints.lg = breakpoints[2];
 breakpoints.xl = breakpoints[3];
 
-
 export default function App() {
   return (
     <Router>
       <div>
-      <Header/>
+        <Header />
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/" exact>
-            <Home/>
+            <Home />
           </Route>
           <Route path="/about">About</Route>
           <Route path="/users">Users</Route>
-          <Route path="/posts"/>
+          <Route path="/posts" />
         </Switch>
       </div>
     </Router>
