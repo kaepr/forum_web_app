53828d69f8e22944be504af9818d19d1c3ac1a1f
Shagun Agrawal <agrawalshagun07@gmail.com>
2021-04-12 00:07:04 +0530

    add schema

---
+++ client/src/component/Post/IndividualPost.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * Post Replies Schema
+ * UUID, UserID, SID, Description, Title, Likes
+ *
+ */
+
+const IndividualPost = () => {
+  return <div>Individual Post </div>;
+};
+
+export default IndividualPost;
