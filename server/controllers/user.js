1923e5b5bf4064ce4b7d63494cfd5c49aa22feb8
Shagun Agrawal <agrawalshagun07@gmail.com>
2021-04-13 17:34:46 +0530

    fix get user info

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,11 +1,17 @@
 import mysql from 'mysql2/promise';
+import { dbConfig } from '../config/db';
 
 export const getUserInfo = async (req, res) => {
   try {
-    console.log(req);
-    console.log('Req User ID: ', req.userId);
+    const user_uuid = req.userId;
+    const getUserQuery = 'Select * from userdata where UUID=?';
+
+    const connection = await mysql.createConnection(dbConfig);
+    const [userRows] = await connection.execute(getUserQuery, [user_uuid]);
+    const userData = userRows[0];
+
     return res.status(200).json({
-      uuidUser: req.userId
+      data: userData
     });
   } catch (err) {
     return res.status(500).json({
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -4,14 +4,12 @@ import { JWT_SECRET } from '../config/secrets';
 export const isLogged = (req, res, next) => {
   try {
     const token = req.cookies.token;
-    console.log('token = ', token);
-    if (!token) return res.status(401).json({ msg: 'Guy Not authorized' });
+
+    if (!token) return res.status(401).json({ msg: 'Not authorized' });
 
     const verified = jwt.verify(token, JWT_SECRET);
 
     req.userId = verified.userId;
-
-    console.log(req);
 
     return next();
   } catch (err) {
